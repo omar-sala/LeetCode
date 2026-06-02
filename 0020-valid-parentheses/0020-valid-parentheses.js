ee82e1090df80124dbd59ee5c61224efbf4aa7b5
Omar Salama <107924846+omar-sala@users.noreply.github.com>
2026-06-02 20:47:32 +0300

    Time: 2 ms (83.21%), Space: 56.3 MB (35.08%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let st = [];
+    for(let i=0; i<s.length; i++){
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            st.push(s[i]);
+        } else{
+            if (st.length > 0 &&
+                ((st[st.length - 1] === '(' && s[i] === ')') ||
+                 (st[st.length - 1] === '{' && s[i] === '}') ||
+                 (st[st.length - 1] === '[' && s[i] === ']'))) {
+
+                // Pop the matching opening bracket
+                st.pop(); 
+            } else{
+                return false;
+            }
+        }
+    }
+    return st.length === 0;
+};
