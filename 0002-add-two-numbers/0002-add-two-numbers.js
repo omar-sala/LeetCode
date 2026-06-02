6c86f234761eb5784529dc30ce1fe19dd8a1089e
Omar Salama <107924846+omar-sala@users.noreply.github.com>
2026-06-02 19:00:45 +0300

    Time: 2 ms (86%), Space: 61.5 MB (52.39%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -11,5 +11,18 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    
+    let dummy = new ListNode();
+    let current = dummy;
+    let carry = 0;
+
+    while (l1 || l2 || carry) {
+        let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+    }
+
+    return dummy.next;
 };
