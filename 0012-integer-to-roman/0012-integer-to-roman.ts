254820ba2f9c33f1142ab0cdee31fca48badd93d
Omar Salama <107924846+omar-sala@users.noreply.github.com>
2026-06-09 15:31:49 +0300

    Time: 2 ms (99.35%), Space: 61.6 MB (81.18%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.ts
@@ -0,0 +1,24 @@
+function intToRoman(num: number): string {
+    const values: number[] = [
+    1000, 900, 500, 400,
+    100, 90, 50, 40,
+    10, 9, 5, 4, 1
+  ];
+
+  const romans: string[] = [
+    "M", "CM", "D", "CD",
+    "C", "XC", "L", "XL",
+    "X", "IX", "V", "IV", "I"
+  ];
+
+  let result = "";
+
+  for (let i = 0; i < values.length; i++) {
+    while (num >= values[i]) {
+      result += romans[i];
+      num -= values[i];
+    }
+  }
+
+  return result;
+};
